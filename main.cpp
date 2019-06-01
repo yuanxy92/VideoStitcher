04a402e64c69b47a79a54e64a4d7be36b3a3ef31
ShaneYuan <yuanxy92@gmail.com>
2019-06-01 16:29:07 +0800

    small modification

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main(int argc, char* argv[]) {
 	std::string dir = "E:/data/giga_paper/360/5+9_G+L_0530";
 	std::vector<cv::VideoCapture> readers(5);
 	cv::VideoWriter writer;
-	readers[0].open(cv::format("%s/MIX_CUCAU1731012_CUCAU1725012.mp4", dir.c_str()));
-	readers[1].open(cv::format("%s/MIX_CUCAU1731016_CUCAU1829016.mp4", dir.c_str()));
-	readers[2].open(cv::format("%s/MIX_CUCAU1731017_CUCAU1724014.mp4", dir.c_str()));
-	readers[3].open(cv::format("%s/MIX_CUCAU1829012_CUCAU1731034.mp4", dir.c_str()));
-	readers[4].open(cv::format("%s/MIX_CUCAU1829020_CUCAU1731023.mp4", dir.c_str()));
+	readers[0].open(cv::format("%s/CUCAU1731012_CUCAU1725012_Master.mp4", dir.c_str()));
+	readers[1].open(cv::format("%s/CUCAU1731016_CUCAU1829016_Master.mp4", dir.c_str()));
+	readers[2].open(cv::format("%s/CUCAU1731017_CUCAU1724014_Master.mp4", dir.c_str()));
+	readers[3].open(cv::format("%s/CUCAU1829012_CUCAU1731034_Master.mp4", dir.c_str()));
+	readers[4].open(cv::format("%s/CUCAU1829020_CUCAU1731023_Master.mp4", dir.c_str()));
 
 	cv::Ptr<mycv::VideoStitcher> stitcher = mycv::VideoStitcher::create();
 	cv::Mat pano;
